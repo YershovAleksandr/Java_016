51482c65de8d5d34ccfbfa12dfd079bd222230fc
hp <namstudionsk@gmail.com>
2018-05-10 21:06:16 +0700

    Add all files

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args){
+        PrintWriter pw = new PrintWriter(System.out, true);
+
+        pw.println("Hello world ПИДОР Bitch!!");
+
+
+    }
+}
