56f78f2df4c1e17ff522ebf271e97fded5607cff
hp <namstudionsk@gmail.com>
2018-05-31 14:09:50 +0700

    Add some staff

--- src/main/java/StrTo.java
+++ src/main/java/StrTo.java
@@ -5,5 +5,16 @@ public class StrTo {
         PrintWriter pw = new PrintWriter(System.out, true);
 
         pw.println("Str 2");
+
+        String result = String.join("~", "ЙAlpha", "Beta", "Gamma");
+        pw.println("Result " + result);
+
+        pw.println("Wtf " + result.codePointAt(0));
+
+        StringBuffer sb = new StringBuffer(result);
+
+        pw.println("Lenght = " + sb.length() + " Capacity = " + sb.capacity());
+        sb.trimToSize();
+        pw.println("Lenght = " + sb.length() + " Capacity = " + sb.capacity());
     }
 }
