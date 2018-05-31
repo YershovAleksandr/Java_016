9e923ac7bc744074489d9f02a835f809a0d8a1c7
hp <namstudionsk@gmail.com>
2018-05-31 12:04:13 +0700

    Add some staff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.io.PrintWriter;
 public class Main {
     public static void main(String[] args){
 
-        Str str = new Str();
+        //Str str = new Str();
         //str.run();
 
         StrTo str2 = new StrTo();
